b4f1fa147fc19b688c13dbf76d36755659ad36ff
Aileen Pohl <aileenn.pohl@gmail.com>
2018-06-09 22:19:19 +0200

    first static product pages

---
+++ static/js/carousel.js
@@ -0,0 +1,11 @@
+
+
+
+(function($) {
+  "use strict"; // Start of use strict
+
+$('.carousel-item').click(function() {
+  $('#adidas_carousel').carousel('prev');
+});
+
+})(jQuery); // End of use strict
